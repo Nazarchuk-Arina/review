f5d588298a19113d554799c79686e9e600f62378
Arina Nazarchuk <nazarchuk.arina@gmail.com>
2026-02-06 11:44:09 +0200

    add all

--- js/script.js
+++ js/script.js
@@ -1,45 +1,20 @@
-'use strict';
+const buttons = document.querySelectorAll('button'),
+      wrapper = document.querySelector('.btn-block');
 
-const box = document.getElementById("box");
-const btns = document.getElementsByTagName('button');
-const circles = document.getElementsByClassName('circle');
-const hearts = document.querySelectorAll(".heart");
-const heart = document.querySelector(".heart") 
-const wrapper = document.querySelector('.wrapper');
-// hearts.forEach(item => {
-//     console.log(item);
-// });
-box.style.backgroundColor = 'blue';
-box.style.width = '100px';
-box.style.cssText = "background-color: blue; width: 100px"
+console.log(buttons[0].classList.add('red'));
+console.log(buttons[0].classList.remove('blue'));
+console.log(buttons[0].classList.toggle('blue'));
 
-btns[1].style.borderRadius = '100%';
-circles[0].style.backgroundColor = 'red';
-
-for (let i = 0; i < hearts.length; i++) {
-    hearts[i].style.backgroundColor = 'blue';
-}
-
-hearts.forEach(item => {
-    item.style.backgroundColor = 'black';
+buttons[0].addEventListener('click', () => {
+    if (!buttons[1].classList.contains('red')) {
+        buttons[1].classList.add('red');
+    } else {
+        buttons[1].classList.remove('red');
+    }
 });
 
-const div = document.createElement('div');
-// const text = document.createTextNode('тут был я');
-
-div.classList.add('black');
-
-// document.body.append(div);
-wrapper.append(div);
-// wrapper.appendChild(div);
-// wrapper.prepend(div);
-
-// hearts[0].before(div); 
-// hearts[0].after(div); 
-
-// circles[0].remove();
-hearts[0].replaceWith(circles[0]);
-
-div.innerHTML = '<h1>hello world</h1>';
-// div.textContent =  'hello';
-div.insertAdjacentHTML('beforebegin', '<h2>hi</h2>') 
+wrapper.addEventListener('click', (event) => {
+if (event.target && event.target.tagName === 'BUTTON') {
+    console.log('hello');
+}
+});
